464d9694554016a2ba9f47fe47746a9d98a4f926
AMAURYS DE JESUS SANCHEZ MEJIA <20170327@unapec.edu.do>
2018-05-16 13:58:44 -0400

    Agregar archivos de proyecto.

---
+++ Models/addModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SumaDosNumeros.Models
+{
+    public class addModel
+    {
+        public int firstNumber { get; set; }
+        public int secondNumber { get; set; }
+        public int resultNumber { get; set; }
+    }
+}
